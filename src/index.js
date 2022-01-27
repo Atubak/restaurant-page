2cd1d36a3e12722fc39f983046754dd4058fa0a2
Jona <mr-jona@hotmail.com>
2022-01-27 18:12:47 +0100

    made a start with the tab modules and switched around some elements

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
+import "./style.css";
 import "./initial-page";
+import imageLocation from './restaurant.jpg';
 
+const buildTab = (() => {
+    const contentDiv = document.querySelector('#content');
+
+
+    let tabDiv = document.createElement('div');
+    let buttonDiv = document.createElement('div');
+    let btn1 = document.createElement('button');
+    let btn2 = document.createElement('button');
+    let btn3 = document.createElement('button');
+    let img = new Image();
+
+    tabDiv.id = 'tab';
+    btn1.classList = 'btn';
+    btn2.classList = 'btn';
+    btn3.classList = 'btn';
+    btn1.id = 'btn1';
+    btn2.id = 'btn2';
+    btn3.id = 'btn3';
+    btn1.innerHTML = 'Home';
+    btn2.innerHTML = 'Menu';
+    btn3.innerHTML = 'Contact';
+    img.src = imageLocation;
+
+
+    buttonDiv.appendChild(btn1);
+    buttonDiv.appendChild(btn2);
+    buttonDiv.appendChild(btn3);
+    tabDiv.appendChild(img);
+    
+    document.getElementById('initial').appendChild(buttonDiv);
+    contentDiv.appendChild(tabDiv);
+})();
+
+
+const changeTab = (() => {
+    
+    window.addEventListener('click', change);
+    
+    
+    function change(e) {
+        document.querySelector('#tab').innerHTML = '';
+        
+        console.log(`lol yeah ${e.target.textContent}`);
+
+    };
+
+})();
--- src/initial-page.js
+++ src/initial-page.js
@@ -1,24 +1,17 @@
-import imageLocation from './restaurant.jpg';
-import "./style.css";
 
 const contentDiv = document.querySelector('#content');
 
 let initialDiv = document.createElement('div');
-let tabDiv = document.createElement('div');
 let h1 = document.createElement('h1');
-let img = new Image();
 let p = document.createElement('p');
 
 initialDiv.id = 'initial';
-tabDiv.id = 'tab';
 h1.innerHTML = "Joni's Macaroni";
-img.src = imageLocation;
-p.innerHTML = "<p>Since 1992, our guests have come through our doors to enjoy a well-made cocktail, a diverse selection of wines and most importantly, an unforgettable meal. It’s these elements and the intangibles that have enticed them to return again and again – our warm and welcoming atmosphere, the friendly and familiar service, the excitement and of course, the personal greeting at the door.<br>Whether you're bringing the entire family for Sunday Dinner, your partner for date night, your client for a lunch meeting for the first time or for the 100th, our promise to you remains true - to provide an effortless and memorable experience.</p>";
+p.innerHTML = "Since 1992, our guests have come through our doors to enjoy a well-made cocktail, a diverse selection of wines and most importantly, an unforgettable meal. It’s these elements and the intangibles that have enticed them to return again and again – our warm and welcoming atmosphere, the friendly and familiar service, the excitement and of course, the personal greeting at the door.<br>Whether you're bringing the entire family for Sunday Dinner, your partner for date night, your client for a lunch meeting for the first time or for the 100th, our promise to you remains true - to provide an effortless and memorable experience.";
 
 initialDiv.appendChild(h1);
 initialDiv.appendChild(p);
-tabDiv.appendChild(img);
 
 contentDiv.appendChild(initialDiv);
-contentDiv.appendChild(tabDiv);
+
 
